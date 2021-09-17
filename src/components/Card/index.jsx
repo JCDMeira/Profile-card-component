7fdf10a3a590ea4a2a1a2604b4f4268301716efd
Jean Meira <jean.meira10@hotmail.com>
2021-09-17 14:49:58 -0300

    Uppdate: card styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -10,6 +10,29 @@ function Card() {
       <div className="circle">
         <img className="photo" src={photo} alt="" />
       </div>
+
+      <div className="info">
+        <div className="pessornalInfo">
+          <h1 className="name">Victor Crest</h1>
+          <h1 className="age">26</h1>
+        </div>
+        <p className="local">London</p>
+      </div>
+
+      <div className="allData">
+        <div>
+          <h1 className="data">80k</h1>
+          <p>Followers</p>
+        </div>
+        <div>
+          <h1 className="data">803k</h1>
+          <p>Likes</p>
+        </div>
+        <div>
+          <h1 className="data">1.4k</h1>
+          <p>Photos</p>
+        </div>
+      </div>
     </div>
   );
 }
